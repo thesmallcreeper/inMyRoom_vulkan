2a303b039a3b58d2af90e613b0014263ec01dc32
thesmallcreeper <nikostpt@hotmail.com>
2018-11-24 03:44:52 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rem A small script that build (or rebuild) AMD Anvil project/solution and OpenGLMathimatics(glm) files using cmake
 
-cd Anvil-master
+cd Anvil
 if exist "build" rmdir /s/q build
 mkdir build
 cd build
@@ -9,7 +9,7 @@ cmake -G "Visual Studio 15 2017 Win64" ..
 cd ..
 cd ..
 
-cd glm-master
+cd glm
 if exist "build" rmdir /s/q build
 mkdir build
 cd build
