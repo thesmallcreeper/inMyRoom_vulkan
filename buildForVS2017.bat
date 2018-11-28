0582f37705848d7369f9b11c50331df9ff7d4de5
Snoopy <nikostpt@hotmail.com>
2018-11-28 19:37:00 +0200

    Add CMAKE file and making GLM just-header style

--- buildForVS2017.bat
+++ buildForVS2017.bat
@@ -6,12 +6,4 @@ mkdir build
 cd build
 cmake -G "Visual Studio 15 2017 Win64" ..
 
-cd ..
-cd ..
-
-cd glm
-if exist "build" rmdir /s/q build
-mkdir build
-cd build
-cmake -G "Visual Studio 15 2017 Win64" ..
 pause
