e47fbc2089f7de930e3c1fcd2c4cd4e0684ebeac
Le creepeur petitè <nikostpt@hotmail.com>
2022-06-04 17:18:42 +0300

    Update WindowWithAsyncInput.cpp

--- inMyRoom_vulkan/src/WindowWithAsyncInput.cpp
+++ inMyRoom_vulkan/src/WindowWithAsyncInput.cpp
@@ -25,6 +25,13 @@ WindowWithAsyncInput::WindowWithAsyncInput(const std::string&             in_tit
 
                 glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
                 window = glfwCreateWindow(in_width, in_height, in_title.c_str(), nullptr, nullptr);
+                glfwSetWindowPos(window, 0, 0);
+                glfwSetWindowAttrib(window, GLFW_RESIZABLE, GLFW_FALSE);
+                glfwSetWindowAttrib(window, GLFW_VISIBLE, GLFW_TRUE);
+                glfwSetWindowAttrib(window, GLFW_FLOATING, GLFW_TRUE);
+                glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
+                glfwSetWindowAttrib(window, GLFW_SCALE_TO_MONITOR, GLFW_FALSE);
+                glfwSetWindowAttrib(window, GLFW_AUTO_ICONIFY, GLFW_FALSE);
 
 #ifdef NDEBUG // -------
                 glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
