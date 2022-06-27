35670cc46ef7583e19f3a78f813a2abc9ba8066f
Le creepeur petitè <nikostpt@hotmail.com>
2022-06-27 20:07:06 +0300

    Update samplesPosition.glsl

--- inMyRoom_vulkan/shaders/common/samplesPosition.glsl
+++ inMyRoom_vulkan/shaders/common/samplesPosition.glsl
@@ -12,77 +12,55 @@ vec2 InputSamplesPositions(int i) {
     #if !defined(SAMPLES_COUNT)
         return vec2(0.5f, 0.5f);
     #elif SAMPLES_COUNT == 2
-        switch (i) {
-            case 0:
-                return vec2(0.75f, 0.75f);
-            case 1:
-                return vec2(0.25f, 0.25f);
-        }
+        vec2 samples_pos[2] = {
+            vec2(0.75f, 0.75f),
+            vec2(0.25f, 0.25f)
+        };
+
+        return samples_pos[i];
     #elif SAMPLES_COUNT == 4
-        switch (i) {
-            case 0:
-                return vec2(0.375f, 0.125f);
-            case 1:
-                return vec2(0.875f, 0.375f);
-            case 2:
-                return vec2(0.125f, 0.625f);
-            case 3:
-                return vec2(0.625f, 0.875f);
-        }
+        vec2 samples_pos[4] = {
+            vec2(0.375f, 0.125f),
+            vec2(0.875f, 0.375f),
+            vec2(0.125f, 0.625f),
+            vec2(0.625f, 0.875f)
+        };
+
+        return samples_pos[i];
     #elif SAMPLES_COUNT == 8
-        switch (i) {
-            case 0:
-                return vec2(0.5625, 0.3125);
-            case 1:
-                return vec2(0.4375, 0.6875);
-            case 2:
-                return vec2(0.8125, 0.5625);
-            case 3:
-                return vec2(0.3125, 0.1875);
-            case 4:
-                return vec2(0.1875, 0.8125);
-            case 5:
-                return vec2(0.0625, 0.4375);
-            case 6:
-                return vec2(0.6875, 0.9375);
-            case 7:
-                return vec2(0.9375, 0.0625);
-        }
+        vec2 samples_pos[8] = {
+            vec2(0.5625, 0.3125),
+            vec2(0.4375, 0.6875),
+            vec2(0.8125, 0.5625),
+            vec2(0.3125, 0.1875),
+            vec2(0.1875, 0.8125),
+            vec2(0.0625, 0.4375),
+            vec2(0.6875, 0.9375),
+            vec2(0.9375, 0.0625)
+        };
+
+        return samples_pos[i];
     #elif SAMPLES_COUNT == 16
-        switch (i) {
-            case 0:
-                return vec2(0.5625, 0.5625);
-            case 1:
-                return vec2(0.4375, 0.3125);
-            case 2:
-                return vec2(0.3125, 0.625);
-            case 3:
-                return vec2(0.75, 0.4375);
-            case 4:
-                return vec2(0.1875, 0.375);
-            case 5:
-                return vec2(0.625, 0.8125);
-            case 6:
-                return vec2(0.8125, 0.6875);
-            case 7:
-                return vec2(0.6875, 0.1875);
-            case 8:
-                return vec2(0.375, 0.875);
-            case 9:
-                return vec2(0.5, 0.0625);
-            case 10:
-                return vec2(0.25, 0.125);
-            case 11:
-                return vec2(0.125, 0.75);
-            case 12:
-                return vec2(0.0, 0.5);
-            case 13:
-                return vec2(0.9375, 0.25);
-            case 14:
-                return vec2(0.875, 0.9375);
-            case 15:
-                return vec2(0.0625, 0.0);
-        }
+        vec2 samples_pos[16] = {
+            vec2(0.5625, 0.5625),
+            vec2(0.4375, 0.3125),
+            vec2(0.3125, 0.625),
+            vec2(0.75, 0.4375),
+            vec2(0.1875, 0.375),
+            vec2(0.625, 0.8125),
+            vec2(0.8125, 0.6875),
+            vec2(0.6875, 0.1875),
+            vec2(0.375, 0.875),
+            vec2(0.5, 0.0625),
+            vec2(0.25, 0.125),
+            vec2(0.125, 0.75),
+            vec2(0.0, 0.5),
+            vec2(0.9375, 0.25),
+            vec2(0.875, 0.9375),
+            vec2(0.0625, 0.0)
+        };
+
+        return samples_pos[i];
     #endif
 }
 
